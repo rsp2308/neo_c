ccfb8fba8bbdcdee6b388687c049ad5e57c8895f
rsp2308 <roshan.parmar2024@lpu.in>
2025-02-01 12:42:41 +0530

    curated

--- unit2/q2.c
+++ unit2/q2.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int roadType, speed;
     printf("Enter the road type (1 for Residential, 2 for City, 3 for Highway): ");
     scanf("%d", &roadType);
     printf("Enter the current speed: ");
     scanf("%d", &speed);
 
-    printf("%s", (speed < (roadType == 1 ? 25 : roadType == 2 ? 35 : 55)) ? "Below" : (speed == (roadType == 1 ? 25 : roadType == 2 ? 35 : 55)) ? "Normal" : "Above");
+    printf("%s", (speed < (roadType == 1 ? 25 : roadType == 2 ? 35
+                                                              : 55))
+                     ? "Below"
+                 : (speed == (roadType == 1 ? 25 : roadType == 2 ? 35
+                                                                 : 55))
+                     ? "Normal"
+                     : "Above");
     return 0;
 }
